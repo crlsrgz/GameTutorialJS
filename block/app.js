f7e5db8dce09f57f633f2c7714732b0d08aaa2d8
crlsrgz <c@c.com>
2022-08-04 11:57:51 +0200

    func: Added user with movement, left and right

--- block/app.js
+++ block/app.js
@@ -3,6 +3,9 @@ const grid = document.querySelector('.grid')
 const blockLength = 100;
 const blockHeight = 20;
 
+const userStart = [230,10];
+let currentPosition = userStart;
+
 
 class Block {
     constructor(xAxis, yAxis){
@@ -20,6 +23,16 @@ const blocks = [
     new Block(230, 270),
     new Block(340, 270),
     new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
 ]
 
 
@@ -36,11 +49,37 @@ function createBlocks(){
 
 createBlocks();
 
+// User
+
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
+
+//draw user
+function drawUser(){
+    user.style.left = currentPosition[0] +"px";
+    user.style.bottom = currentPosition[1] +"px";
+
+}
 
 
+//move user
 
+function moveUser(event){
+    switch(event.key){ // event.keyCOde:39
+        case 'ArrowLeft': 
+            currentPosition[0] -= 10
+            drawUser();            
+            break;
+        case 'ArrowRight': 
+            currentPosition[0] += 10
+            drawUser();            
+            break;
+    }
+}
 
-
+document.addEventListener("keydown", moveUser);
 
 
 
